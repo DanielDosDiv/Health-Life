668cd2e76fe4632ac69f491ac43ea5b3f229f119
Daniel Souza <danieldesouza1024@gmail.com>
2024-09-20 08:18:23 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var btn = document.getElementById("proximo");
+var slider = document.querySelector(".slider");
+
+btn.addEventListener("click", ()=>{
+    slider.classList.toggle("active")
+
+})
+function HomePage() {
+    window.location.href =  "HomePage.html";
+}
